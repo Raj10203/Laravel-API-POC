9a0791894760fd3e7cf4ee8e369e616aa6925911
Raj10203 <patilricky39@gmail.com>
2025-08-11 12:20:05 +0530

    login-logout with email is implimented

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -3,9 +3,59 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\UserRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    //
+    public function signup(UserRequest $request)
+    {
+        $request->validated();
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+
+        return response()->json([
+            'status' => true,
+            'data' => $user,
+        ], 200);
+
+    }
+
+    public function login(LoginRequest $request)
+    {
+
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            $authUser = Auth::user();
+            return response()->json([
+                'status' => true,
+                'message' => 'User Logged in Successfully',
+                'token' => $authUser->createToken('API Token')->plainTextToken,
+                'token_type' => 'bearer'
+            ], 200);
+        } else {
+            return response()->json([
+            'status' => false,
+            'message' => 'Emial & Password does not matched',
+        ], 403);
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        $user = $request->user();
+        $user->tokens()->delete();
+
+         return response()->json([
+                'status' => true,
+                'date' => $user,
+                'message' => 'User Logged out Successfully',
+            ], 200);
+    }
 }
