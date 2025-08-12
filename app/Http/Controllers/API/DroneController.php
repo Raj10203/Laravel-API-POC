0a2d3d52612f0310dc28c30da7a5148ddee1e8fc
Raj10203 <patilricky39@gmail.com>
2025-08-12 13:46:02 +0530

    drone, inspection required files created

---
+++ app/Http/Controllers/API/DroneController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Drone;
+use Illuminate\Http\Request;
+
+class DroneController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Drone $drone)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Drone $drone)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Drone $drone)
+    {
+        //
+    }
+}
